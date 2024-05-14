ee2aa8c192f132965346d3cd71ed5e2f10113193
Kashish Goyal <kashish1782004@gmail.com>
2024-05-15 00:33:16 +0530

    extracting links from web page

--- extract_links_web_page.py
+++ extract_links_web_page.py
@@ -2,3 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 web=requests.get("https://www.tutorialsfreak.com/")
 soup=BeautifulSoup(web.content,"html.parser")
+# print(soup.find_all("a"))
+for i in soup.find_all("a"):
+    print(i.get("href"))

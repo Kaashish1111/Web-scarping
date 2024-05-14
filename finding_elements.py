3d9e75442c4ccdb6eaac4641b9cf9c396047d475
Kashish Goyal <kashish1782004@gmail.com>
2024-05-15 00:03:48 +0530

    more on web scarpping

--- finding_elements.py
+++ finding_elements.py
@@ -1,2 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+web=requests.get("https://www.tutorialsfreak.com/")
+soup=BeautifulSoup(web.content,"html.parser")
+print(soup.prettify())
+print("\n")
+class_data=soup.find("div",class_="bg-white border-0 p-3 card-footer")
+# for id just use id=
+print(class_data.prettify())
